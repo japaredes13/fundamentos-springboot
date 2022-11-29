5a40e837446cf9ed0c9ea600ed10fbe0c0fabcde
Javier <jparedes@roshka.com>
2022-11-29 10:50:49 -0300

    fix de versiones spring boot y java respectivamente. Pruebas de GetMapping

--- src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
+++ src/main/java/com/fundamentos/springboot/fundamentos/FundamentosApplication.java
@@ -2,12 +2,21 @@ package com.fundamentos.springboot.fundamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FundamentosApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FundamentosApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+		return String.format("Hello %s!", name);
 	}
 
 }
